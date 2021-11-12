bc902d59750b702a96caddfad8e7c3bd59be2b17
Mahdi <mahdi.gerami77@gmail.com>
2021-11-12 16:56:17 +0330

    make website pwa

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,20 @@
-import nodeResolve from '@rollup/plugin-node-resolve';
-import babel from '@rollup/plugin-babel';
-import html from '@web/rollup-plugin-html';
-import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
-import { terser } from 'rollup-plugin-terser';
-import { generateSW } from 'rollup-plugin-workbox';
-import path from 'path';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import html from "@web/rollup-plugin-html";
+import { importMetaAssets } from "@web/rollup-plugin-import-meta-assets";
+import { terser } from "rollup-plugin-terser";
+import { generateSW } from "rollup-plugin-workbox";
+import path from "path";
+import copy from "rollup-plugin-copy";
 
 export default {
-  input: 'index.html',
+  input: "index.html",
   output: {
-    entryFileNames: '[hash].js',
-    chunkFileNames: '[hash].js',
-    assetFileNames: '[hash][extname]',
-    format: 'es',
-    dir: 'dist',
+    entryFileNames: "[hash].js",
+    chunkFileNames: "[hash].js",
+    assetFileNames: "[hash][extname]",
+    format: "es",
+    dir: "dist",
   },
   preserveEntrySignatures: false,
 
@@ -22,7 +23,7 @@ export default {
     html({
       minify: true,
       injectServiceWorker: true,
-      serviceWorkerPath: 'dist/sw.js',
+      serviceWorkerPath: "dist/sw.js",
     }),
     /** Resolve bare module imports */
     nodeResolve(),
@@ -32,16 +33,16 @@ export default {
     importMetaAssets(),
     /** Compile JS to a lower language target */
     babel({
-      babelHelpers: 'bundled',
+      babelHelpers: "bundled",
       presets: [
         [
-          require.resolve('@babel/preset-env'),
+          require.resolve("@babel/preset-env"),
           {
             targets: [
-              'last 3 Chrome major versions',
-              'last 3 Firefox major versions',
-              'last 3 Edge major versions',
-              'last 3 Safari major versions',
+              "last 3 Chrome major versions",
+              "last 3 Firefox major versions",
+              "last 3 Edge major versions",
+              "last 3 Safari major versions",
             ],
             modules: false,
             bugfixes: true,
@@ -50,9 +51,9 @@ export default {
       ],
       plugins: [
         [
-          require.resolve('babel-plugin-template-html-minifier'),
+          require.resolve("babel-plugin-template-html-minifier"),
           {
-            modules: { lit: ['html', { name: 'css', encapsulation: 'style' }] },
+            modules: { lit: ["html", { name: "css", encapsulation: "style" }] },
             failOnError: false,
             strictCSS: true,
             htmlMinifier: {
@@ -68,15 +69,19 @@ export default {
     }),
     /** Create and inject a service worker */
     generateSW({
-      navigateFallback: '/index.html',
+      navigateFallback: "/index.html",
       // where to output the generated sw
-      swDest: path.join('dist', 'sw.js'),
+      swDest: path.join("dist", "sw.js"),
       // directory to match patterns against to be precached
-      globDirectory: path.join('dist'),
+      globDirectory: path.join("dist"),
       // cache any html js and css by default
-      globPatterns: ['**/*.{html,js,css,webmanifest}'],
+      globPatterns: ["**/*.{html,js,css,webmanifest}"],
       skipWaiting: true,
       clientsClaim: true,
+    }),
+
+    copy({
+      targets: [{ src: "assets", dest: "dist" }],
     }),
   ],
 };
